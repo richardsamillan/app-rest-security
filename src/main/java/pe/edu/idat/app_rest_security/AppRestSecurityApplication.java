f4db522a8126452f1983082705d73729152232cc
richardsamillan <richiander24@gmail.com>
2025-08-06 12:12:52 -0500

    proyecto security se12

---
+++ src/main/java/pe/edu/idat/app_rest_security/AppRestSecurityApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_rest_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRestSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_rest_security/AppRestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_rest_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
